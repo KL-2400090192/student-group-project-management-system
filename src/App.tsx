182a327c8806a53621ce95b1d44c127732e7c04b
KL-2400090192 <tummalahimesh@gmail.com>
2026-02-24 23:09:48 +0530

    Remove Toaster component from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
-import { Toaster } from './components/ui/sonner';
 
 export default function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-      <Toaster />
-    </>
+    <RouterProvider router={router} />
   );
 }

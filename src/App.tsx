a35280f023712ba8c835fc4c264efc02c5ebd878
KL-2400090192 <tummalahimesh@gmail.com>
2026-02-24 23:05:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
-import { UserProvider } from './context/UserContext';
 import { Toaster } from './components/ui/sonner';
 
 export default function App() {
   return (
-    <UserProvider>
+    <>
       <RouterProvider router={router} />
       <Toaster />
-    </UserProvider>
+    </>
   );
 }
